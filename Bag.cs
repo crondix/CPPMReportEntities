0a0119e74a499fc90fd5d26113ea29f9592b53f6
Алексей Дударев <Dudarev.Aleksey@orenmin.onmicrosoft.com>
2025-03-18 15:43:00 +0500

    Добавьте файлы проекта.

---
+++ Bag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPPMReportEntities
+{
+    public class bag
+    {
+        public Guid Id { get; set; }
+        public FabricType type { get; set; }
+        public double Weight { get; set; }
+        
+
+    }
+}
